f6b62e09d464d7bf810a3c52d5f3b4a5eadfb3f3
Jonathan M. <neodev7440@gmail.com>
2026-03-30 11:32:25 +0200

    ajout de la fonction de recherche par mot clé

--- src/main/java/com/example/dao/ArticleRepository.java
+++ src/main/java/com/example/dao/ArticleRepository.java
@@ -1,7 +1,10 @@
 package com.example.dao;
 
 import com.example.entities.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleRepository extends JpaRepository<Article,Long> {
+    Page<Article> findByDescriptionContains(String description,Pageable pageable);
 }
--- src/main/java/com/example/web/ArticleController.java
+++ src/main/java/com/example/web/ArticleController.java
@@ -3,9 +3,12 @@ package com.example.web;
 import com.example.dao.ArticleRepository;
 import com.example.entities.Article;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,9 +18,13 @@ public class ArticleController {
     private ArticleRepository articleRepository;
 
     @GetMapping("/index")
-    public String index(Model model) {
-        List<Article> articles = articleRepository.findAll();
-        model.addAttribute("listArticle", articles);
+    public String index(Model model, @RequestParam(name = "page", defaultValue = "0") int page,
+                                     @RequestParam(name="keyword", defaultValue = "")String kw) {
+        Page<Article> articles = articleRepository.findByDescriptionContains(kw,PageRequest.of(page,5));
+
+        model.addAttribute("listArticle", articles.getContent());
+        model.addAttribute("pages", new int[articles.getTotalPages()]);
+        model.addAttribute("currentPage", page);
         return "articles";
     }
 }
