f2e89a3f3d4da9ec793f7760be3eb1bc9b7ac92d
Jonathan M. <neodev7440@gmail.com>
2026-03-30 10:57:10 +0200

    ajout de bootstrap"
    ²

--- src/main/java/com/example/web/ArticleController.java
+++ src/main/java/com/example/web/ArticleController.java
@@ -1,9 +1,13 @@
 package com.example.web;
 
 import com.example.dao.ArticleRepository;
+import com.example.entities.Article;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class ArticleController {
@@ -11,7 +15,9 @@ public class ArticleController {
     private ArticleRepository articleRepository;
 
     @GetMapping("/index")
-    public String index() {
+    public String index(Model model) {
+        List<Article> articles = articleRepository.findAll();
+        model.addAttribute("listArticle", articles);
         return "articles";
     }
 }
