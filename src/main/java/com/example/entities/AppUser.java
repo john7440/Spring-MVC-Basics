bb9bc5167f87fabbf9cd8555e28ab0ede68ca311
Jonathan M. <neodev7440@gmail.com>
2026-03-31 13:11:30 +0200

    ajout de l'entité appuser

---
+++ src/main/java/com/example/entities/AppUser.java
@@ -0,0 +1,23 @@
+package com.example.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity
+@Data
+@AllArgsConstructor @NoArgsConstructor
+public class AppUser implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String role;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private Cart cart;
+}
