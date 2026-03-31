3e75f74bb5eb19b35bc1a56b659fc1fec5b1e20e
Jonathan M. <neodev7440@gmail.com>
2026-03-31 13:03:33 +0200

    ajout de l'entité cartitem

---
+++ src/main/java/com/example/entities/CartItem.java
@@ -0,0 +1,28 @@
+package com.example.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity
+@Data
+@AllArgsConstructor @NoArgsConstructor
+public class CartItem implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "article_id")
+    private Article article;
+
+    private int quantity;
+}
